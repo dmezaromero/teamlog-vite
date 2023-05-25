2ae4564c518c3f0da034e4855b3bc898a21d6608
Dagoberto Meza <dagoberto.meza@spacelab.cr>
2023-05-25 15:53:58 -0600

    fix 1.4

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+      <h1>Home Page</h1>
+      <p>Welcome to the home page!</p>
+      <p>Test</p>
+    </div>
+  </template>
+
+<script>
+export default {
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
